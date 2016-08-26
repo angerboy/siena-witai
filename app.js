0671396d3fa48ffe79026eee590997900e72ab16
Matt Ehinger <mattehinger@Matts-MacBook-Pro-2.local>
2016-08-26 10:32:53 -0500

    set port manually

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
 
+app.set('port', process.env.PORT || 3000);
+
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
 app.listen(3000, function () {
-    console.log('Node app listening on port 3000!');
+    console.log('Node app listening on port ', app.get('port'));
 });
