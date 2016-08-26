1bdc2438e056b81e51f2fbf6b9a3855e58143ece
Matt Ehinger <mattehinger@Matts-MacBook-Pro-2.local>
2016-08-26 10:37:45 -0500

    add public page

--- app.js
+++ app.js
@@ -2,11 +2,8 @@ var express = require('express');
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
+app.use(express.static('public'));
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
-
-app.listen(3000, function () {
+app.listen(app.get('port'), function () {
     console.log('Node app listening on port ', app.get('port'));
 });
