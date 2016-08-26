5d79cd19b3ecf5d51e1ce52ce26c0feaaa8ff6cc
Matt Ehinger <mattehinger@Matts-MacBook-Pro-2.local>
2016-08-26 11:43:15 -0500

    add sessions add wit-ai module

---
+++ sessions/sessions.js
@@ -0,0 +1,21 @@
+
+
+
+/**
+ * Find the current session in DynamoDB or create one if none exist
+ * @param facebookID
+ * @returns the current session
+ */
+function findOrCreateSession(facebookID) {
+    // query DynamoDB for current session
+    // if found return session
+    // else create session and return
+}
+
+/**
+ * Sets/updates session in DynamoDB
+ */
+function setSession() {
+
+}
+
