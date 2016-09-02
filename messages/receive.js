08c36881f7e14f51df524c8a1c7b5a7baea3c3ad
Matt Ehinger <mattehinger@Matts-MacBook-Pro-2.local>
2016-09-02 15:53:39 -0500

    send okay

--- messages/receive.js
+++ messages/receive.js
@@ -65,8 +65,8 @@ function receivedPostbackFromMessenger(req, res) {
  */
 function receivedInputFromClient(req, res) {
     console.log("Received input from client");
-    console.log(res);
     //res.send("Received your input");
+    res.send("got it bro");
     witai.callWitAIWithRes(res, req.body.message.text);
 }
 
